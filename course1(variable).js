7300aadde2f208e6a9d98348fc42c3265a04766b
AchhyutB <achhyut.baral03@gmail.com>
2025-05-05 09:32:15 +0545

    first commit

---
+++ course1(variable).js
@@ -0,0 +1,41 @@
+
+
+
+
+
+// const student = {
+//   fullName: "AB",
+//   age: 21,
+//   cgpa: 6.5,
+//   isPass: true,
+// };
+
+// console.log(student["age"]);
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//  Practice qn
+
+// const product = {
+//   title: "ball pen",
+//   rating: 4.5,
+//   offer: 5,
+//   price: 270,
+// };
+// console.log(product);
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Practice qn 2
+
+// const profile = {
+//   profileName: "abc",
+//   isFollow: false,
+//   post: 195,
+//   followers: 598000,
+//   following: 4,
+//   entrepreneur: "apna college",
+// };
+
+// console.log(profile);
+// console.log(typeof profile["post"]);
---
+++ course2(operator).js
@@ -0,0 +1,85 @@
+console.log("hello world");
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Arithemic Operator
+
+// let a = 5;
+// let b = 2;
+// console.log("a+b=", a + b);
+// console.log("a-b=", a - b);
+// console.log("a*b=", a * b);
+// console.log("a/b=", a / b);
+// console.log("a%b=", a % b);
+// console.log("a**b=", a ** b); ------//  (**) means a^b
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Unary Operator
+
+// let a = 5;
+// let b = 2;
+// console.log("a=", a, "& b=", b);
+// a++;
+// console.log("a++=", a);  //----------Post Increment  and pre increment will be ++a
+// a--;
+// console.log("a--=", a);  //----------Post Decrement  and pre decrement will be --a
+
+//post increment and decrement take display the value before the a++/a--
+//pre increment and decrement take display the value after the ++a/--a
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Assignment Operators -------( =, +=, -=, *=, %= **=)
+
+// let a = 5;
+// let b = 5;
+// let c = 5;
+// let d = 5;
+// let e = 5;
+// let f = 5;
+// let g = 5;
+
+// a += 4; // means a=a+4
+// console.log("a=", a); //9
+
+// b -= 4; // means b=b-4
+// console.log("b=", b);  //1
+
+// c *= 4; // means c=c*4
+// console.log("c=", c);  //20
+
+// d /= 4; // means d=d/4
+// console.log("d=", d);  //1.25
+
+// e %= 4; // means e=e%4
+// console.log("e=", e);  //1
+
+// f **= 4; // means f=f**4
+// console.log("f=", f);  //625
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Comparison Operators   (== equal to,   !=  not equal to,            === equal to & type, !==  not equal to & type)
+
+// let a = 5;
+// let b = 2;
+
+// console.log("5==2", a == b);  //false
+
+// let c = 5;
+// let d = 2;
+
+// console.log("c!=d=", c != d);   //true
+
+// let e = 5;
+// let f = 2;
+
+//console.log("e===f", e === f);//false true come only both value is same
+
+// let g = 5;
+// let h = "2"; // string to number
+
+// console.log("g!==h", g !== h); //true if the value of the h=5 then the result will be false
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
