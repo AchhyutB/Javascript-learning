2394e0eacbc9668d3a0a02761a6f8d7839293d79
AchhyutB <achhyut.baral03@gmail.com>
2025-05-12 13:05:27 +0545

    idk

--- course3(loop).js
+++ course3(loop).js
@@ -101,3 +101,6 @@ console.log("hello world");
 //PRACTICE QN
 
 //1. Print all the even number from 0 to 100.
+
+for (let num = 0; num <= 100; num)
+  if (num % 2 == 0) console.log("the even number from 1 to 100 are:", num);
