10fab9c99087659ce5e253db443e0ff42a026631
AchhyutB <achhyut.baral03@gmail.com>
2025-05-16 19:49:24 +0545

    Some syntax in Function

--- course5(function).js
+++ course5(function).js
@@ -1,1 +1,20 @@
 console.log("Hello\nWorld");
+
+// Function in JS
+
+// Block of code that performs a specifics task, can be invoked whenever needed.
+
+// Function Defination(syntax)
+
+// Function functionName(){
+//do some work
+//}
+
+// Function Call
+
+// functionName();
+
+
+// function functionName(parameter1, parameter2,....){
+// do some work
+//}
