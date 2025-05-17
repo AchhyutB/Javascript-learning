2441c47a80eeb29c849255d8df6e4c27429de113
AchhyutB <achhyut.baral03@gmail.com>
2025-05-17 18:17:11 +0545

    About Arrow Function

--- course5(function).js
+++ course5(function).js
@@ -31,3 +31,82 @@ console.log("Hello\nWorld");
 //     console.log("learning JS");
 // }
 // myFunction();                    // Calling function
+
+
+// Sum of two value.
+
+
+// function num() {
+//   let a = prompt("Enter 1st number");
+//   let b = prompt("Enter 2nd number");
+  
+//   // Convert inputs to numbers
+//   let sum = Number(a) + Number(b);
+  
+//   return sum;
+// }
+
+// let result = num();
+// console.log("Sum:", result);
+
+
+
+// Arrow Function   "=>" compact way of writing function.
+
+
+
+// const functionName=(parameter1, parameter2...) =>{
+    // do some work
+    //}
+
+//example
+
+//const sum(a,b) =>{
+    //return a+b:
+    //}
+
+// Practice Qn.
+
+// Qno 1.
+
+// Create a function using the "function"keyword that takes a string as an argument and returns the number of vowels in the string.
+
+
+// function countVowels(str) {
+//     let count=0;
+//   for (const char of str) {
+//     if (
+//         char === "a" || 
+//         char === "e" ||
+//         char === "i" ||
+//         char === "o" ||
+//         char === "u" 
+//     )   {
+//         count++;
+//     }
+// }
+//     console.log(count);
+//   }
+
+
+
+
+// Qn no 2.
+// Create an arrow function to perform the same task.
+
+// const countVow = (str) => { 
+//     let count=0;
+//   for (const char of str) {
+//     if (
+//         char === "a" || 
+//         char === "e" ||
+//         char === "i" ||
+//         char === "o" ||
+//         char === "u" 
+//     )   {
+//         count++;
+//     }
+// }
+//     console.log(count);
+  
+// } 
