6b694e7e06f50d8d6b224bc6eae07d4df8fb957f
AchhyutB <achhyut.baral03@gmail.com>
2025-05-19 20:38:00 +0545

    for(dom-1)

--- course6(dom-part1).js
+++ course6(dom-part1).js
@@ -1,1 +1,18 @@
 console.log("Hello\nWorld");
+
+// 3 musketeers of Web Dev
+
+// 1. HTML, 2. CSS, 3. JavaScript
+
+// Window Object                                                                                                                                                         The window object represents an open window in a browser. It is browser's object(not JS's) and is automatically created by browser. It is a global object with lots of properties and methods.
+
+
+
+// DOM( Document Object Model)
+
+// When a web page is loaded, the browser creates a DOM of the page.
+
+
+//how it works
+
+// HTML---> DOCUMENT---> HTML---> 1.HEAD-->meta, meta, title, link.  2.BODY---> DIV->img, h1, p, div, span...  SCRIPT---> Script
