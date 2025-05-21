6504c0afafd9cbcef22c64181bf63d3e0f6341e9
AchhyutB <achhyut.baral03@gmail.com>
2025-05-21 22:14:34 +0545

    About Attributes

--- course6(dom-part2).js
+++ course6(dom-part2).js
@@ -1,1 +1,33 @@
 console.log("Hello\nWorld");
+
+//Attributes
+
+//getAttribute(attr) // to get the attribute value
+
+
+// let div=document.querySelector("div");
+// console.log(div);
+// let id = div.getAttribute("id");
+// console.log(id);
+// let names=div.getAttribute("name");
+// console.log(names);
+
+// let para=document.querySelector("p");
+// console.log(para.getAttribute("class"));
+
+
+
+
+//setAttribute(attr, value) // to set the attribute val th
+
+//  let para=document.querySelector("p");
+//  console.log(para.setAttribute("class","newClass"));
+
+
+//Styles
+
+//node.style-->(to access css through JS)
+
+// let div=document.querySelector("div");
+// div.style.backgroundColor ="purple";
+// div.style.fontSize="14px";
