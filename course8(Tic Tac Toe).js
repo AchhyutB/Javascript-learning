37475e803b124f282b27ee12be28f9cbe8fdcd66
AchhyutB <achhyut.baral03@gmail.com>
2025-05-26 21:42:24 +0545

    Making the winner display when X or O wins.

--- course8(Tic Tac Toe).js
+++ course8(Tic Tac Toe).js
@@ -1,5 +1,8 @@
 let boxes=document.querySelectorAll(".box");
 let reset=document.querySelector("#Reset");
+let newGameBtn=document.querySelector("#new-btn");
+let msgContainer=document.querySelector(".msg-container");
+let msg=document.querySelector("#msg");
 
 let turnO = true // playerX, playerO
 
@@ -30,6 +33,22 @@ boxes.forEach((box)=>{
         checkWinner();
     });
 });
-const checkWinner=()=>{
-     
+
+const showWinner = (winner) => {
+    msg.innerText=`Congratulations,Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
 }
+const checkWinner=()=>{
+     for(let pattern of winPatterns){
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if(pos1Val === pos2Val && pos2Val === pos3Val){
+                console.log("winner",pos1Val);
+                showWinner(pos1Val);
+            }
+        }
+     }
+};
