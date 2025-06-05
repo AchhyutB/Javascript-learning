fe1c80bcd52605214ce514cb78a8694a6a552455
AchhyutB <achhyut.baral03@gmail.com>
2025-06-05 18:07:02 +0545

    learn Async-Await and IIFE in JS

--- course10(callback).js
+++ course10(callback).js
@@ -36,7 +36,9 @@
 // calculator(1,2, sum);
 
 
-// CALLBACK HELL : Nested callbacks stacked below one another forming a pyramid structure.(pyramid of doom). This style of programming becomes difficult to understand and manage.
+// CALLBACK HELL : 
+
+// Nested callbacks stacked below one another forming a pyramid structure.(pyramid of doom). This style of programming becomes difficult to understand and manage.
 
 
 // function getData(dataId, getNextData){
@@ -61,6 +63,9 @@
 // });
 
 
+
+
+
 // Promises
 
 // Promises is for "eventual" completion of task. It is an object in JS. It is a solution to callback hell.
@@ -77,6 +82,9 @@
 //     //or
 //     //reject("it's be rejected");
 // });
+
+
+
 
 
 // .then() and .catch()
@@ -114,6 +122,9 @@
 // });
 
 
+
+
+
 // Promise chain
 
 
@@ -136,4 +147,73 @@
 
 
 
+
+
+
 // Async-Await
+
+// async function always returns a promise
+
+// async function myFunc(){...}   await pauses the execution of its surrounding async function until the promise is settled.
+
+
+
+
+// function api(){
+//     return new Promise((resolve, reject)=>{
+//        setTimeout(()=>{
+//         console.log("weather data");
+//         resolve(200);
+//        },2000)
+//     })
+// }
+// async function getWeatherData(){
+//     await api(); //1st call
+//     await api(); //2nd call
+// }
+
+
+
+
+
+// function getData(dataId){
+//  return new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//         console.log("data",dataId)
+//         resolve("success");
+//     },2000);
+//  });
+// }
+// async function getAllData() {
+//     console.log("getting data1....");
+//     await getData(1);
+//     console.log("getting data2....");
+//     await getData(2);
+//     console.log("getting data3....");
+//     await getData(3);
+//     console.log("getting data4....");
+//     await getData(4);
+//     console.log("getting data5....");
+//     await getData(5);
+//     console.log("getting data6....");
+//     await getData(6);
+// }
+
+
+// IIFE  : Immediately Invoked Function Expression
+//         IIFE is a function that is called immediately as soon as it is defined
+
+
+//syntax
+
+
+// (function(){
+     //....
+// })();
+
+// (()=>{
+     //....
+// })();
+// (async()=>{
+     //....
+// })();
